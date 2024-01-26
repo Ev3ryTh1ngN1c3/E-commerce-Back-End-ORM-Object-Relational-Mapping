1912e5f87fbe62fe2297504b741f0c11e221479a
Lisa <143395934+Ev3ryTh1ngN1c3@users.noreply.github.com>
2024-01-26 18:52:58 -0500

    imported models from their files to index.js

--- models/index.js
+++ models/index.js
@@ -4,14 +4,29 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
-// Categories have many Products
+// categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 
-// Products belongToMany Tags (through ProductTag)
+// products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+});
 
-// Tags belongToMany Products (through ProductTag)
+// tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+});
 
+// export the models
 module.exports = {
   Product,
   Category,
