f1e3b33dadb882296983892bda69056deffe44f8
Lisa <143395934+Ev3ryTh1ngN1c3@users.noreply.github.com>
2024-01-26 18:43:54 -0500

    definded missing columns for the Category model

--- models/Category.js
+++ models/Category.js
@@ -1,14 +1,28 @@
+// import the necessary modules
 const { Model, DataTypes } = require('sequelize');
-
 const sequelize = require('../config/connection.js');
 
+// create the category model by extending the sequelize model class
 class Category extends Model {}
 
+// initialize the Category model with the column definitions
 Category.init(
   {
-    // define columns
+    // define the 'id' column
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    // Define the 'category_name' column
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
+    // set the sequelize instance & other options
     sequelize,
     timestamps: false,
     freezeTableName: true,
@@ -17,4 +31,5 @@ Category.init(
   }
 );
 
+// export the category model
 module.exports = Category;
