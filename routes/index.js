c5deab49ae69535f01107bb6848554eb3769101e
Lisa <143395934+Ev3ryTh1ngN1c3@users.noreply.github.com>
2024-01-26 20:14:10 -0500

    added comments to the index.js file

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 
+// use the '/api' prefix for all routes defined in the 'apiRoutes' module
 router.use('/api', apiRoutes);
 
+// catch-all route for any other routes that are not defined
 router.use((req, res) => {
-  res.send("<h1>Wrong Route!</h1>")
+  res.send("<h1>Wrong Route!</h1>");
 });
 
 module.exports = router;
