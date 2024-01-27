d04a069b14cdf0dfdbf80d0d7f3d5f84c03348d9
Lisa <143395934+Ev3ryTh1ngN1c3@users.noreply.github.com>
2024-01-26 19:54:16 -0500

    added comments to the index.js file

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,8 +3,13 @@ const categoryRoutes = require('./category-routes');
 const productRoutes = require('./product-routes');
 const tagRoutes = require('./tag-routes');
 
+// import & use the category routes
 router.use('/categories', categoryRoutes);
+
+// import & use the product routes
 router.use('/products', productRoutes);
+
+// import & use the tag routes
 router.use('/tags', tagRoutes);
 
 module.exports = router;
