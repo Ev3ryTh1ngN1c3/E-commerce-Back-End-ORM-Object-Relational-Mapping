fa21a4cd27fafc7bbc9d6e3b6e8d851018366876
Lisa <143395934+Ev3ryTh1ngN1c3@users.noreply.github.com>
2024-01-26 20:21:15 -0500

    added comments the seeds index.js file

--- seeds/index.js
+++ seeds/index.js
@@ -1,26 +1,37 @@
+// import seed functions for categories, products, tags & product tags
 const seedCategories = require('./category-seeds');
 const seedProducts = require('./product-seeds');
 const seedTags = require('./tag-seeds');
 const seedProductTags = require('./product-tag-seeds');
 
+// import the sequelize connection
 const sequelize = require('../config/connection');
 
+// function to seed the database with all data
 const seedAll = async () => {
+  // sync the database by dropping & recreating tables
   await sequelize.sync({ force: true });
   console.log('\n----- DATABASE SYNCED -----\n');
+
+  // seed categories
   await seedCategories();
   console.log('\n----- CATEGORIES SEEDED -----\n');
 
+  // seed products
   await seedProducts();
   console.log('\n----- PRODUCTS SEEDED -----\n');
 
+  // seed tags
   await seedTags();
   console.log('\n----- TAGS SEEDED -----\n');
 
+  // seed product tags
   await seedProductTags();
   console.log('\n----- PRODUCT TAGS SEEDED -----\n');
 
+  // exit the process
   process.exit(0);
 };
 
+// call the seedAll function to start seeding the database
 seedAll();
