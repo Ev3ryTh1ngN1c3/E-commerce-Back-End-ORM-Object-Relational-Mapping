d871c248072f29383f16432ecc8ffd4a4ddf6410
Lisa <143395934+Ev3ryTh1ngN1c3@users.noreply.github.com>
2024-01-26 19:33:26 -0500

    defined the ProductTag model file with 3 columns

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,14 +1,39 @@
+// import necessary modules
 const { Model, DataTypes } = require('sequelize');
-
 const sequelize = require('../config/connection');
 
+// define the ProductTag model
 class ProductTag extends Model {}
 
+// initialize the ProductTag model with column definitions
 ProductTag.init(
   {
-    // define columns
+    // define the id column
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    // define the product_id column as a foreign key referencing the product model's id column
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id',
+      },
+    },
+    // define the tag_id column as a foreign key referencing the tag model's id column
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id',
+      },
+    },
   },
   {
+    // configure the model options
     sequelize,
     timestamps: false,
     freezeTableName: true,
@@ -17,4 +42,5 @@ ProductTag.init(
   }
 );
 
+// export the ProductTag model
 module.exports = ProductTag;
